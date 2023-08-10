afa9b9bc8d2caa8851f15ffcdffbae4502165743
bhartivikas <bhartivikas2427@gmail.com>
2023-08-10 20:50:34 +0530

    Changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'employee-portal-client'`, () => {
+  it(`should have as title 'tryeasy-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('employee-portal-client');
+    expect(app.title).toEqual('tryeasy-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('employee-portal-client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tryeasy-ui app is running!');
   });
 });
